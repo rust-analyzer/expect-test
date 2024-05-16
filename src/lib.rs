0416fe200e30fb6630e03b8e1b90c866e6149567
xxchan <xxchan22f@gmail.com>
2024-05-16 22:46:21 +0800

    update test
    Signed-off-by: xxchan <xxchan22f@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -855,11 +855,10 @@ line1
             [[r#""]]"#]],
         );
 
-        // Check `expect![[  ]]` as well.
+        // Check `expect![[  ]]`
         assert_eq!(locate_end("[]]"), Some(2));
-        // For `expect![]]`, this will be invalid syntax: Syntax Error: expected SEMICOLON
-        // In `locate_end`, we just find the first `]` is the end position and ignores the rest.
-        assert_eq!(locate_end("]]"), Some(0));
+        // Check `expect![  ]`
+        assert_eq!(locate_end("]"), Some(0));
     }
 
     #[test]
